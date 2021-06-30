4d371d2a44ea9a144f5bf6ac1b7cf41a62d71c14
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-06-30 09:47:47 -0300

    Só falta mexer no baackground

--- src/components/inputAndCards.js
+++ src/components/inputAndCards.js
@@ -2,21 +2,22 @@ import { React, useEffect, useState } from "react"
 import axios from 'axios'
 import '../styles/body.css'
 import '../styles/cards.css'
+import Cards from "./cards"
 
 export default function Input(){
     const [search, setSearch] = useState('')
-    const [datas, setDatas] = useState('')
+    const [datas, setDatas] = useState([])
 
     useEffect(()=>{
         let params = {}
         if(search){
             params = search
+            axios.get(`https://www.googleapis.com/books/v1/volumes?q=${params}`)
+                .then((res) => {
+                    setDatas(res.data.items)    
+                    console.log(res.data.items)
+                })
         }
-        axios.get(`https://www.googleapis.com/books/v1/volumes?q=${params}`)
-            .then((res) => {
-                setDatas(res.data)
-                console.log(res.data)
-            })
     }, [search])
     return(
         <main>
@@ -27,33 +28,7 @@ export default function Input(){
                 </form>
             </div>
             <div className='container'>
-                <div>
-                    <h1> Do mil ao milhão</h1>
-                    <h3>Sem cortar o cafezinho</h3>
-                    <img src='https://books.google.com/books/content?id=9P4vAQAAMAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api' alt='Thumbnail this book'></img>
-                    <section>
-                        <h6>Authors: Thiago Nigro</h6>
-                        <p><strong>Description:</strong> Em seu primeiro livro, Thiago Nigro, criador da plataforma O Primo Rico, ensina aos leitores os três pilares para atingir a independência financeira: gastar bem, investir melhor e ganhar mais.</p>
-                    </section>
-                </div>
-                <div>
-                    <h1> Do mil ao milhão</h1>
-                    <h3>Sem cortar o cafezinho</h3>
-                    <img src='https://books.google.com/books/content?id=9P4vAQAAMAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api' alt='Thumbnail this book'></img>
-                    <section>
-                        <h6>Authors: Thiago Nigro</h6>
-                        <p><strong>Description:</strong> Em seu primeiro livro, Thiago Nigro, criador da plataforma O Primo Rico, ensina aos leitores os três pilares para atingir a independência financeira: gastar bem, investir melhor e ganhar mais.</p>
-                    </section>
-                </div>
-                <div>
-                    <h1>Do mil ao milhão</h1>
-                    <h3>Sem cortar o cafezinho</h3>
-                    <img src='https://books.google.com/books/content?id=9P4vAQAAMAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api' alt='Thumbnail this book'></img>
-                    <section>
-                        <h6>Authors: Thiago Nigro</h6>
-                        <p><strong>Description:</strong> Em seu primeiro livro, Thiago Nigro, criador da plataforma O Primo Rico, ensina aos leitores os três pilares para atingir a independência financeira: gastar bem, investir melhor e ganhar mais.</p>
-                    </section>
-                </div>
+                <Cards datas={datas} loading={!datas.length}></Cards>
             </div>
         </main>
     )
